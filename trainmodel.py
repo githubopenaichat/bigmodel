93c30a8007068ff52ca0cf04172e18def4626664
githubopenaichat <905868059@qq.com>
2024-09-23 16:57:22 +0800

    Update trainmodel.py

--- trainmodel.py
+++ trainmodel.py
@@ -1,11 +1,43 @@
-from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer,AdamW,get_scheduler,AutoModel
+from huggingface_hub import login, Repository
+from accelerate import Accelerator
+from datasets import load_dataset
+import torch
+from accelerate import Accelerator
+import datasets
+import transformers
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import logging
+import wandb
+
+import torch
+from torch.utils.data import IterableDataset
+
+from argparse import Namespace
 
 org = "transformersbook"
 model_ckpt = "codeparrot"
 
 tokenizer_ = AutoTokenizer.from_pretrained(org+"/"+model_ckpt)
-config_small = AutoConfig.from_pretrained("gpt2-xl", vocab_size=len(tokenizer))
+config_small = AutoConfig.from_pretrained("gpt2-xl", vocab_size=len(tokenizer_))
 model_small_ = AutoModelForCausalLM.from_config(config_small)
+
+config = {"train_batch_size": 2, # 12
+          "valid_batch_size": 2, # 12
+          "weight_decay": 0.1,
+          "shuffle_buffer": 1000,
+          "learning_rate": 2e-4, # 5e-4
+          "lr_scheduler_type": "cosine",
+          "num_warmup_steps": 750, # 2000
+          "gradient_accumulation_steps": 16, # 1
+          "max_train_steps": 50000, # 150000
+          "max_eval_steps": -1,
+          "seq_length": 1024,
+          "seed": 1,
+          "save_checkpoint_steps": 50000} # 15000
+
+args = Namespace(**config)
 
 # 保存模型  
 model_small_.save_pretrained("/root/data/bigmodel/models/codeparrot-small")
@@ -15,6 +47,54 @@ tokenizer_.save_pretrained("/root/data/bigmodel/models/codeparrot-small")
 tokenizer = AutoTokenizer.from_pretrained("/root/data/bigmodel/models/codeparrot-small")
 model = AutoModel.from_pretrained("/root/data/bigmodel/models/codeparrot-small")
 
+
+class ConstantLengthDataset(IterableDataset):    
+    def __init__(self, tokenizer, dataset, seq_length=1024,
+                 num_of_sequences=1024, chars_per_token=3.6):
+        self.tokenizer = tokenizer
+        self.concat_token_id = tokenizer.eos_token_id
+        self.dataset = dataset
+        self.seq_length = seq_length
+        self.input_characters = seq_length * chars_per_token * num_of_sequences
+    
+    def __iter__(self):
+        iterator = iter(self.dataset)
+        more_examples = True
+        while more_examples:
+            buffer, buffer_len = [], 0
+            while True:
+                if buffer_len >= self.input_characters:
+                    m=f"Buffer full: {buffer_len}>={self.input_characters:.0f}"
+                    print(m)
+                    break
+                try:
+                    m=f"Fill buffer: {buffer_len}<{self.input_characters:.0f}"
+                    print(m)
+                    buffer.append(next(iterator)["content"])
+                    buffer_len += len(buffer[-1])
+                except StopIteration:
+                    iterator = iter(self.dataset)
+
+            all_token_ids = []
+            tokenized_inputs = self.tokenizer(buffer, truncation=False)
+            for tokenized_input in tokenized_inputs['input_ids']:
+                all_token_ids.extend(tokenized_input + [self.concat_token_id])
+            
+            for i in range(0, len(all_token_ids), self.seq_length):
+                input_ids = all_token_ids[i : i + self.seq_length]
+                if len(input_ids) == self.seq_length:
+                    yield torch.tensor(input_ids)
+
+def get_grouped_params(model, no_decay=["bias", "LayerNorm.weight"]):
+    params_with_wd, params_without_wd = [], []
+    for n, p in model.named_parameters():
+        if any(nd in n for nd in no_decay):
+            params_without_wd.append(p)
+        else:
+            params_with_wd.append(p)
+    return [{'params': params_with_wd, 'weight_decay': args.weight_decay},
+            {'params': params_without_wd, 'weight_decay': 0.0}]
+            
 def create_dataloaders(dataset_name):
     train_data = load_dataset(dataset_name+'-train', split="train", streaming=True)
     train_data = train_data.shuffle(buffer_size=args.shuffle_buffer, seed=args.seed)
@@ -26,7 +106,10 @@ def create_dataloaders(dataset_name):
     train_dataloader=DataLoader(train_dataset, batch_size=args.train_batch_size)
     eval_dataloader=DataLoader(valid_dataset, batch_size=args.valid_batch_size)
     return train_dataloader, eval_dataloader
-    
+
+dataset_name = "transformersbook/codeparrot"
+accelerator = Accelerator()
+optimizer = AdamW(get_grouped_params(model), lr=args.learning_rate)
 train_dataloader, eval_dataloader = create_dataloaders(dataset_name)
 model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(model, optimizer, train_dataloader, eval_dataloader)
 
