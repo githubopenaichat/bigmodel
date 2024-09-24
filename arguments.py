0ebd8e557dad48fcefcf289430c666199196e079
githubopenaichat <905868059@qq.com>
2024-09-24 10:59:59 +0800

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -1,217 +1,220 @@
-from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer,AdamW,get_scheduler
-from huggingface_hub import login, Repository
-from accelerate import Accelerator
-from datasets import load_dataset
-import torch
-from accelerate import Accelerator
-import datasets
-import transformers
-from torch.utils.data.dataloader import DataLoader
-from torch.utils.tensorboard import SummaryWriter
-import logging
-import wandb
-
-import torch
-from torch.utils.data import IterableDataset
-
-from argparse import Namespace
-
-org = "transformersbook"
-model_ckpt = "codeparrot"
-token = "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"
-login(token)
+from dataclasses import dataclass, field
+from typing import Optional
 
 
+@dataclass
+class TrainingArguments:
+    """
+    Configuration for training model.
+    """
 
-# Commented parameters correspond to the small model
-config = {"train_batch_size": 2, # 12
-          "valid_batch_size": 2, # 12
-          "weight_decay": 0.1,
-          "shuffle_buffer": 1000,
-          "learning_rate": 2e-4, # 5e-4
-          "lr_scheduler_type": "cosine",
-          "num_warmup_steps": 750, # 2000
-          "gradient_accumulation_steps": 16, # 1
-          "max_train_steps": 50000, # 150000
-          "max_eval_steps": -1,
-          "seq_length": 1024,
-          "seed": 1,
-          "save_checkpoint_steps": 50000} # 15000
-
-args = Namespace(**config)
-
-
-class ConstantLengthDataset(IterableDataset):    
-    def __init__(self, tokenizer, dataset, seq_length=1024,
-                 num_of_sequences=1024, chars_per_token=3.6):
-        self.tokenizer = tokenizer
-        self.concat_token_id = tokenizer.eos_token_id
-        self.dataset = dataset
-        self.seq_length = seq_length
-        self.input_characters = seq_length * chars_per_token * num_of_sequences
-    
-    def __iter__(self):
-        iterator = iter(self.dataset)
-        more_examples = True
-        while more_examples:
-            buffer, buffer_len = [], 0
-            while True:
-                if buffer_len >= self.input_characters:
-                    m=f"Buffer full: {buffer_len}>={self.input_characters:.0f}"
-                    print(m)
-                    break
-                try:
-                    m=f"Fill buffer: {buffer_len}<{self.input_characters:.0f}"
-                    print(m)
-                    buffer.append(next(iterator)["content"])
-                    buffer_len += len(buffer[-1])
-                except StopIteration:
-                    iterator = iter(self.dataset)
-
-            all_token_ids = []
-            tokenized_inputs = self.tokenizer(buffer, truncation=False)
-            for tokenized_input in tokenized_inputs['input_ids']:
-                all_token_ids.extend(tokenized_input + [self.concat_token_id])
-            
-            for i in range(0, len(all_token_ids), self.seq_length):
-                input_ids = all_token_ids[i : i + self.seq_length]
-                if len(input_ids) == self.seq_length:
-                    yield torch.tensor(input_ids)
-
-def setup_logging(project_name):
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-        datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO, handlers=[
-        logging.FileHandler(f"log/debug_{accelerator.process_index}.log"),
-        logging.StreamHandler()])
-    if accelerator.is_main_process: # We only want to set up logging once
-        wandb.init(project=project_name, config=args)
-        run_name = wandb.run.name
-        tb_writer = SummaryWriter()
-        tb_writer.add_hparams(vars(args), {'0': 0})
-        logger.setLevel(logging.INFO)
-        datasets.utils.logging.set_verbosity_debug()
-        transformers.utils.logging.set_verbosity_info()
-    else:
-        tb_writer = None
-        run_name = ''
-        logger.setLevel(logging.ERROR)
-        datasets.utils.logging.set_verbosity_error()
-        transformers.utils.logging.set_verbosity_error()
-    return logger, tb_writer, run_name
-
-def log_metrics(step, metrics):
-    logger.info(f"Step {step}: {metrics}")
-    if accelerator.is_main_process:
-        wandb.log(metrics)
-        [tb_writer.add_scalar(k, v, step) for k, v in metrics.items()]
+    model_ckpt: Optional[str] = field(
+        default="codeparrot/codeparrot", metadata={"help": "Model name or path of model to be trained."}
+    )
+    save_dir: Optional[str] = field(
+        default="./", metadata={"help": "Save dir where model repo is cloned and models updates are saved to."}
+    )
+    dataset_name_train: Optional[str] = field(
+        default="codeparrot/codeparrot-clean-train", metadata={"help": "Name or path of training dataset."}
+    )
+    dataset_name_valid: Optional[str] = field(
+        default="codeparrot/codeparrot-clean-valid", metadata={"help": "Name or path of validation dataset."}
+    )
+    train_batch_size: Optional[int] = field(default=2, metadata={"help": "Batch size for training."})
+    valid_batch_size: Optional[int] = field(default=2, metadata={"help": "Batch size for evaluation."})
+    weight_decay: Optional[float] = field(default=0.1, metadata={"help": "Value of weight decay."})
+    shuffle_buffer: Optional[int] = field(
+        default=10000, metadata={"help": "Size of buffer used to shuffle streaming dataset."}
+    )
+    learning_rate: Optional[float] = field(default=2e-4, metadata={"help": "Learning rate fo training."})
+    lr_scheduler_type: Optional[str] = field(default="cosine", metadata={"help": "Learning rate."})
+    num_warmup_steps: Optional[int] = field(
+        default=750, metadata={"help": "Number of warmup steps in the learning rate schedule."}
+    )
+    gradient_accumulation_steps: Optional[int] = field(
+        default=16, metadata={"help": "Number of gradient accumulation steps."}
+    )
+    gradient_checkpointing: Optional[bool] = field(
+        default=True, metadata={"help": "Use gradient checkpointing to reduce memory footprint."}
+    )
+    max_train_steps: Optional[int] = field(default=50000, metadata={"help": "Maximum number of training steps."})
+    max_eval_steps: Optional[int] = field(
+        default=-1, metadata={"help": "Maximum number of evaluation steps. If -1 the full dataset is evaluated."}
+    )
+    seq_length: Optional[int] = field(default=1024, metadata={"help": "Sequence lengths used for training."})
+    seed: Optional[int] = field(default=1, metadata={"help": "Training seed."})
+    save_checkpoint_steps: Optional[int] = field(
+        default=1024,
+        metadata={"help": "Interval to save checkpoints. Measured as number of forward passes not training steps."},
+    )
+    resume_from_checkpoint: Optional[str] = field(
+        default=None, metadata={"help": "States path if the training should continue from a checkpoint folder."}
+    )
+    tokenized: Optional[bool] = field(default=False, metadata={"help": "If True the data is pretokenized."})
 
 
+@dataclass
+class EvaluationArguments:
+    """
+    Configuration for evaluating model.
+    """
 
-def create_dataloaders(dataset_name):
-    train_data = load_dataset(dataset_name+'-train', split="train", streaming=True)
-    train_data = train_data.shuffle(buffer_size=args.shuffle_buffer, seed=args.seed)
-    valid_data = load_dataset(dataset_name+'-valid', split="validation",  streaming=True)
-    
-    train_dataset = ConstantLengthDataset(tokenizer, train_data, seq_length=args.seq_length)
-    valid_dataset = ConstantLengthDataset(tokenizer, valid_data, seq_length=args.seq_length)
-    
-    train_dataloader=DataLoader(train_dataset, batch_size=args.train_batch_size)
-    eval_dataloader=DataLoader(valid_dataset, batch_size=args.valid_batch_size)
-    return train_dataloader, eval_dataloader
-
-def get_grouped_params(model, no_decay=["bias", "LayerNorm.weight"]):
-    params_with_wd, params_without_wd = [], []
-    for n, p in model.named_parameters():
-        if any(nd in n for nd in no_decay):
-            params_without_wd.append(p)
-        else:
-            params_with_wd.append(p)
-    return [{'params': params_with_wd, 'weight_decay': args.weight_decay},
-            {'params': params_without_wd, 'weight_decay': 0.0}]
-
-def evaluate():
-    model.eval()
-    losses = []
-    for step, batch in enumerate(eval_dataloader):
-        with torch.no_grad():
-            outputs = model(batch, labels=batch)
-        loss = outputs.loss.repeat(args.valid_batch_size)
-        losses.append(accelerator.gather(loss))
-        if args.max_eval_steps > 0 and step >= args.max_eval_steps: break
-    loss = torch.mean(torch.cat(losses))
-    try:
-        perplexity = torch.exp(loss)
-    except OverflowError:
-        perplexity = torch.tensor(float("inf"))
-    return loss.item(), perplexity.item()
-
-# set_seed(args.seed)
-
-# Accelerator
-accelerator = Accelerator()
-samples_per_step = accelerator.state.num_processes * args.train_batch_size
-dataset_name,project_name = "codeparrot"
-# Logging
-logger, tb_writer, run_name = setup_logging(project_name.split("/")[1])
-logger.info(accelerator.state)
+    model_ckpt: Optional[str] = field(
+        default="codeparrot/codeparrot", metadata={"help": "Model name or path of model to be evaluated."}
+    )
+    dataset_name: Optional[str] = field(
+        default="codeparrot/codeparrot-clean-valid", metadata={"help": "Name or path of validation dataset."}
+    )
+    batch_size: Optional[int] = field(default=2, metadata={"help": "Batch size used for evaluation."})
+    max_eval_steps: Optional[int] = field(
+        default=-1, metadata={"help": "Maximum number of evaluation steps. If -1 the full dataset is evaluated."}
+    )
+    seq_length: Optional[int] = field(default=1024, metadata={"help": "Length of sequences to be evaluated."})
+    seed: Optional[int] = field(default=1, metadata={"help": "Random seed used for evaluation."})
 
 
-# Load model and tokenizer
-if accelerator.is_main_process:
-    hf_repo = Repository("./", clone_from=project_name, revision=run_name)
-model = AutoModelForCausalLM.from_pretrained("./", gradient_checkpointing=True)
-tokenizer = AutoTokenizer.from_pretrained("./")
+@dataclass
+class HumanEvalArguments:
+    """
+    Configuration for running evaluation on HumanEval dataset.
+    """
 
-# Load dataset and dataloader
-train_dataloader, eval_dataloader = create_dataloaders(dataset_name)
+    model_ckpt: Optional[str] = field(
+        default="codeparrot/codeparrot", metadata={"help": "Model name or path of model to be evaluated."}
+    )
+    num_workers: Optional[int] = field(default=None, metadata={"help": "Number of workers used for code evaluation."})
+    num_tasks: Optional[int] = field(
+        default=None,
+        metadata={"help": "The number of human-eval tasks to run. If not included all tasks are evaluated."},
+    )
+    do_sample: Optional[bool] = field(
+        default=True, metadata={"help": "Sample from the language model's output distribution."}
+    )
+    temperature: Optional[float] = field(default=0.2, metadata={"help": "Sampling temperature used for generation."})
+    max_new_tokens: Optional[int] = field(default=256, metadata={"help": "Maximum number of newly generated tokens."})
+    top_k: Optional[int] = field(default=0, metadata={"help": "Top-k parameter used for generation."})
+    top_p: Optional[float] = field(default=0.95, metadata={"help": "Top-p parameter used for nucleus sampling."})
+    batch_size: Optional[int] = field(default=10, metadata={"help": "Number of generations to run in parallel."})
+    n_samples: Optional[int] = field(
+        default=200, metadata={"help": "Number of completions to generate for each sample."}
+    )
+    seed: Optional[int] = field(default=1, metadata={"help": "Random seed used for evaluation."})
+    output_file: Optional[str] = field(
+        default="eval_results.json", metadata={"help": "Random seed used for evaluation."}
+    )
+    HF_ALLOW_CODE_EVAL: Optional[str] = field(
+        default="0", metadata={"help": "Allow `code_eval` to execute Python code on machine"}
+    )
+    device_int: Optional[int] = field(
+        default=-1,
+        metadata={
+            "help": (
+                "Determine which device to run the `text-generation` Pipeline on. -1 is CPU and any zero or positive"
+                " number corresponds to which GPU device id to run on."
+            )
+        },
+    )
 
-# Prepare the optimizer and learning rate scheduler
-optimizer = AdamW(get_grouped_params(model), lr=args.learning_rate)
-lr_scheduler = get_scheduler(name=args.lr_scheduler_type, optimizer=optimizer,
-                             num_warmup_steps=args.num_warmup_steps,
-                             num_training_steps=args.max_train_steps,)
-def get_lr():
-    return optimizer.param_groups[0]['lr']
 
-# Prepare everything with our `accelerator` (order of args is not important)
-model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(
-    model, optimizer, train_dataloader, eval_dataloader)
+@dataclass
+class PreprocessingArguments:
+    """
+    Configuration for preprocessing data.
+    """
 
-# Train model
-model.train()
-completed_steps = 0
-for step, batch in enumerate(train_dataloader, start=1):
-    loss = model(batch, labels=batch).loss
-    log_metrics(step, {'lr': get_lr(), 'samples': step*samples_per_step,
-                       'steps': completed_steps, 'loss/train': loss.item()})
-    loss = loss / args.gradient_accumulation_steps
-    accelerator.backward(loss)
-    if step % args.gradient_accumulation_steps == 0:
-        optimizer.step()
-        lr_scheduler.step()
-        optimizer.zero_grad()
-        completed_steps += 1
-    if step % args.save_checkpoint_steps == 0:
-        logger.info('Evaluating and saving model checkpoint')
-        eval_loss, perplexity = evaluate()
-        log_metrics(step, {'loss/eval': eval_loss, 'perplexity': perplexity})
-        accelerator.wait_for_everyone()
-        unwrapped_model = accelerator.unwrap_model(model)
-        if accelerator.is_main_process:
-            unwrapped_model.save_pretrained("./")
-            hf_repo.push_to_hub(commit_message=f'step {step}')
-        model.train()
-    if completed_steps >= args.max_train_steps:
-        break
+    num_workers: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": "The number of CPU cores to use for parallel preprocessing. Default uses the maximum available."
+        },
+    )
+    dataset_name: Optional[str] = field(
+        default="transformersbook/codeparrot", metadata={"help": "Folder or name of dataset to process."}
+    )
+    output_dir: Optional[str] = field(
+        default="codeparrot-clean", metadata={"help": "Folder to save processed dataset."}
+    )
+    samples_per_file: Optional[int] = field(
+        default=100_000, metadata={"help": "Number of files to save per JSON output file."}
+    )
+    text_column: Optional[str] = field(default="content", metadata={"help": "Column containing text data to process."})
+    line_max: Optional[float] = field(
+        default=1000, metadata={"help": "Maximum line length in file, otherwise file is filtered."}
+    )
+    line_mean: Optional[float] = field(
+        default=100, metadata={"help": "Maximum mean line length in file, otherwise file is filtered."}
+    )
+    alpha_frac: Optional[float] = field(
+        default=0.25, metadata={"help": "Maximum fraction of non-alphanumeric characters, otherwise file is filtered."}
+    )
+    min_token_ratio: Optional[float] = field(
+        default=1.5, metadata={"help": "Minimum character token ratio for the file, otherwise file is filtered."}
+    )
+    filter_proba: Optional[float] = field(
+        default=0.7, metadata={"help": "Probability for filtering config, test and uncommon files."}
+    )
+    tokenizer: Optional[str] = field(
+        default="codeparrot/codeparrot",
+        metadata={"help": "Name or path to the tokenizer."},
+    )
+    near_deduplication: Optional[bool] = field(
+        default=False, metadata={"help": "If True, near-duplicate samples are removed."}
+    )
+    jaccard_threshold: Optional[float] = field(
+        default=0.85, metadata={"help": "Jaccard threshold for near-duplicate samples."}
+    )
 
-# Evaluate and save the last checkpoint
-logger.info('Evaluating and saving model after training')
-eval_loss, perplexity = evaluate()
-log_metrics(step, {'loss/eval': eval_loss, 'perplexity': perplexity})
-accelerator.wait_for_everyone()
-unwrapped_model = accelerator.unwrap_model(model)
-if accelerator.is_main_process:
-    unwrapped_model.save_pretrained("./")
-    hf_repo.push_to_hub(commit_message=f'final model')
+
+@dataclass
+class TokenizerTrainingArguments:
+    """
+    Configuration for tokenizer training.
+    """
+
+    base_tokenizer: Optional[str] = field(
+        default="openai-community/gpt2", metadata={"help": "Base tokenizer to build new tokenizer from."}
+    )
+    dataset_name: Optional[str] = field(
+        default="transformersbook/codeparrot-train", metadata={"help": "Dataset to train tokenizer on."}
+    )
+    text_column: Optional[str] = field(default="content", metadata={"help": "Column containing text data to process."})
+    vocab_size: Optional[int] = field(default=200_000, metadata={"help": "Number of examples to train tokenizer on."})
+    n_examples: Optional[int] = field(
+        default=32768, metadata={"help": "Number of examples to train the tokenizer on."}
+    )
+    tokenizer_name: Optional[str] = field(default="codeparrot", metadata={"help": "Name of new tokenizer."})
+    push_to_hub: Optional[bool] = field(default=True, metadata={"help": "Push saved tokenizer to the hub."})
+
+
+@dataclass
+class PretokenizationArguments:
+    """
+    Configuration for data pretokenization.
+    """
+
+    tokenizer_dir: Optional[str] = field(
+        default="codeparrot/codeparrot", metadata={"help": "Name or path to the tokenizer."}
+    )
+    dataset_name: Optional[str] = field(
+        default="codeparrot/codeparrot-clean-train", metadata={"help": "Name or path to the dataset to pretokenize."}
+    )
+    tokenized_data_repo: Optional[str] = field(
+        default="tokenized-codeparrot-train", metadata={"help": "Repo name of the pretokenized data."}
+    )
+    num_workers: Optional[int] = field(default=None, metadata={"help": "Number of workers used for code evaluation."})
+
+
+@dataclass
+class InitializationArguments:
+    """
+    Configuration for initializing new model.
+    """
+
+    config_name: Optional[str] = field(
+        default="openai-community/gpt2-large", metadata={"help": "Configuration to use for model initialization."}
+    )
+    tokenizer_name: Optional[str] = field(
+        default="codeparrot/codeparrot", metadata={"help": "Tokenizer attached to model."}
+    )
+    model_name: Optional[str] = field(default="codeparrot", metadata={"help": "Name of the created model."})
+    push_to_hub: Optional[bool] = field(default=True, metadata={"help": "Push saved tokenizer to the hub."})
